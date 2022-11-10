202a3e370ae8649f7052112980cbaabeb70707d3
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-11-10 08:56:43 +0530

    Refactor: Ablity To Read and Write The Addressbook

--- src/main/java/com/AddressBook_program/AddressBook.java
+++ src/main/java/com/AddressBook_program/AddressBook.java
@@ -1,119 +1,74 @@
 package com.AddressBook_program;
 
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Scanner;
-import java.util.Set;
 
-import javax.swing.JList;
+import javax.swing.filechooser.FileFilter;
+
 
 public class AddressBook {
-	// hashmap is used to Uniqueness
-	static HashMap<String,String> map = new HashMap<String, String>();
-	static Scanner sc=new Scanner(System.in);
 
-	//   Hashmap Keys pair is used to store the first & last Name 
-	public static String uniquenessData() {
-		System.out.println("First Name Enter");
-		String firstName=sc.next();
-		System.out.println("Last Name Enter");
-		String lastName=sc.next();
-		return firstName+" "+lastName;
+	public void writeAddressBook() throws IOException {
+		ArrayList<String> addrsBook=new ArrayList<String>();
+		addrsBook.add("Nikita");	
+		addrsBook.add("Patil");
+		addrsBook.add("7755889944");
+		addrsBook.add("Aurangabad");
+		addrsBook.add("Maharastra");
+		addrsBook.add("445566");
+		addrsBook.add("nikita@gmail.com");
+		Files.write(Paths.get("C:\\Users\\ASHUTOSH NAWALE\\eclipse-workspace\\AddressBook_Class_199\\AddressBook.txt"),addrsBook, StandardOpenOption.APPEND);
 	}
+	public void readAddressBook() throws IOException, Exception {
+	
+		List<String> lines = Files.readAllLines(Paths.get("C:\\Users\\ASHUTOSH NAWALE\\eclipse-workspace\\AddressBook_Class_199\\AddressBook.txt"));
 
-	// hashmap value pair is used to store the other details of person
-	public static String newAddressBookDataEnter() {
-		System.out.println(" Address Enter");
-		String address=sc.next();
-		System.out.println("PhoneNumber Enter");
-		String phoneNumber=sc.next();
-		System.out.println(" PinCode Enter");
-		String pincode=sc.next();
-		System.out.println(" Email Enter");
-		String email=sc.next();
-		return address+" "+phoneNumber+" "+pincode+" "+email;
-
-	}
-
-	// this method operate all methods
-	public static void storeData() {
-		System.out.println("If address book add Nothing Then Enter The  0");
-		System.out.println("If address book add Details Then Enter The  1");
-		System.out.println("If address book Show Then Enter The  2");
-		int condition=sc.nextInt();
-		switch(condition) {
-
-		case(0): System.out.println("Your address Book Is  Save & Closed");
-		break;
-		//   this case is used to find the uniqueness and fill the detail of person
-		case(1):  
-			int count=0;
-		String unique=uniquenessData();
-		Set<String> keys1 = map.keySet();
-		for (Object key : keys1)
-			if(key.equals(unique)) {
-				System.out.println("Name is Alredy Given in Address Book");
-				count++;
-				break;
-			}
-		if(count==0) {
-			map.put(unique, newAddressBookDataEnter());
-		}
-		storeData();
-		break;
-		//    this case used to print the all hashmap or addressbook details
-		case(2): Set<String> keys2 = map.keySet();
-		for (Object key : keys2)
-			System.out.println("Keys :  "+key+" value:  "+map.get(key));      
-		break;
-		default: System.out.println("Enter Correct Input");
-		}
+		// Printing Lines Read
+		for (String line : lines) {
+			System.out.println(line);
+		}		
 	}
 	
-
-
-    // Method to sort hashmap by values
-    public static HashMap<String, String> sortByValue(HashMap<String, String> hm)
-    {
-        // Create a list from elements of HashMap
-        List<Map.Entry<String, String> > list =
-               new LinkedList<Map.Entry<String, String> >(hm.entrySet());
- 
-        // Sort the list
-        Collections.sort(list, new Comparator<Map.Entry<String, String> >() {
-            public int compare(Map.Entry<String, String> o1,
-                               Map.Entry<String, String> o2)
-            {
-                return (o1.getValue()).compareTo(o2.getValue());
-            }
-        });
-         
-        // put data from sorted list to hashmap
-        HashMap<String, String> temp = new LinkedHashMap<String, String>();
-        for (Map.Entry<String, String> aa : list) {
-            temp.put(aa.getKey(), aa.getValue());
-        }
-        return temp;
-    }
- 
-	
-	public static void main(String[] args) {	
+//	FileReader fileReader =null;
+//	try {
+//		fileReader=new FileReader(infile);
+//		int ch;
+//		while((ch=fileReader.read())!= -1) {
+//			System.out.print((char)ch);
+//		}
+//	}
+//	catch (FileNotFoundException fnfe)
+//    {
+//        System.out.println("NO Such File Exists");
+//    }
+//    catch (IOException except)
+//    {
+//        System.out.println("IOException occurred");
+//    }
+//	  finally {
+//		  fileReader.close(); 
+//	}
+//	}
+//		
+	public static void main(String[] args) throws Exception {	
+		AddressBook obj=new AddressBook();
+		obj.writeAddressBook();
+		obj.readAddressBook();
 		
-		AddressBook.storeData();
-		Map<String, String> sortMap =sortByValue(map);
-		 // print the sorted hashmap
-        for (Map.Entry<String, String> map1 : sortMap.entrySet()) {
-            System.out.println("Key = " + map1.getKey() +
-                          ", Value = " + map1.getValue());
-        }
+		
 	}
 }
---
+++ src/main/java/com/AddressBook_program/DataCollect.java
@@ -0,0 +1,23 @@
+package com.AddressBook_program;
+
+public class DataCollect {
+String firstName;
+String lastName;
+String mobileNo;
+String city;
+String state;
+String zip;
+String email;
+public DataCollect(String firstName, String lastName, String mobileNo, String city, String state, String zip,
+		String email) {
+	super();
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.mobileNo = mobileNo;
+	this.city = city;
+	this.state = state;
+	this.zip = zip;
+	this.email = email;
+}
+
+}
